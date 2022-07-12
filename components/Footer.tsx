d92c3b5dfbd02aaa9e74bbda1326b15cfc0b8a66
ADOUKO Franck Venance <fvadouko@live.fr>
2022-07-12 17:13:46 +0000

    correction of styling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next';
 import { footerList1, footerList2, footerList3 } from '../utils/constants';
 
 const List = ({ items, mt }: { items: string[], mt: Boolean }) => (
-    <div className={`flex flex-wrap gap-1 ${mt && 'mt-1'}`}>
+    <div className={`flex flex-wrap gap-2 ${mt && 'mt-3'}`}>
       {items.map((item: string) => (
         <p key={item} className='text-gray-400 text-sm  hover:underline cursor-pointer' >
           {item}
@@ -14,11 +14,11 @@ const List = ({ items, mt }: { items: string[], mt: Boolean }) => (
 
 const Footer = () => {
   return (
-    <div className='mt-3 hidden xl:block'>
+    <div className='mt-6 hidden xl:block'>
     <List items={footerList1} mt={false} />
     <List items={footerList2} mt={true} />
     <List items={footerList3} mt={true} />
-    <p className='text-gray-400 text-sm mt-1'>© 2022 TikTik</p>
+    <p className='text-gray-400 text-sm mt-5'>© 2022 TikTik</p>
   </div>
   )
 }
