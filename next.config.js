28b38dd864452f5291b317b049625d5931019366
ADOUKO Franck Venance <fvadouko@live.fr>
2022-07-13 16:43:04 +0000

    Interaction with the database of sanity using the api of nextjs.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+//const nextConfig = {
+  //reactStrictMode: true,
+  //swcMinify: true,
+//}
 
-module.exports = nextConfig
+//module.exports = nextConfig
+
+const STUDIO_REWRITE = {
+  source: "/studio/:path*",
+  destination:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3333/studio/:path*"
+      : "/studio/index.html",
+};
+
+module.exports = {
+  rewrites: () => [STUDIO_REWRITE],
+};
