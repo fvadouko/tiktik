0f17180a24e606c8e9650c99acef04b1d017d8e8
ADOUKO Franck Venance <fvadouko@live.fr>
2022-07-13 21:18:59 +0000

    Integration of oauth with google login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,28 +1,42 @@
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { GoogleLogout, GoogleLogin } from 'react-google-login'
-import {AiOutlineLogout} from 'react-icons/ai'
-import {BiSearch} from 'react-icons/bi'
-import {IoMdAdd} from 'react-icons/io'
-import Logo from '../utils/tiktik-logo.png'
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io';
+import Logo from '../utils/tiktik-logo.png';
+import useAuthStore from '../store/authStore';
+import { createOrGetUser } from '../utils';
 
-const  Navbar = () =>{
+const Navbar = () => {
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
   return (
-    <div className='w-full flex justify-between items-center border-b-5 border-gray-200 border-solid py-2 px-4'>
-      <Link href='/'>
-        <div className='w-[100px] md:w-[129px] md:h-[30px] h-[38px]'>
+    <div className="w-full flex justify-between items-center border-b-5 border-gray-200 border-solid py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
           <Image
-            className='cursor-pointer'
+            className="cursor-pointer"
             src={Logo}
-            alt='logo'
-            layout='responsive'
+            alt="logo"
+            layout="responsive"
           />
         </div>
       </Link>
+      <div>BiSearch</div>
+      <div>
+        {userProfile ? (
+          <div>{userProfile?.userName}</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(res) => createOrGetUser(res, addUser)}
+          />
+        )}
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
+import jwtDecode from 'jwt-decode';
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const createOrGetUser = async (response: any, addUser: any) => {
-  var base64Url = response.credential.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
-  
-  const { name, picture, sub } = JSON.parse(jsonPayload)
-  
+  console.log(response)
+  const decoded:{name:string, picture: string, sub:string} =jwtDecode(response.credential);
+  const {name, picture, sub} = decoded
   const user = {
     _id: sub,
     _type: 'user',
@@ -20,5 +16,6 @@ export const createOrGetUser = async (response: any, addUser: any) => {
   
   addUser(user);
 
+  console.log("base_url", BASE_URL)
   await axios.post(`${BASE_URL}/api/auth`, user);
 };
