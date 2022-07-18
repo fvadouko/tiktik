74c09b3ef9608cc55ee4b888d1f1e55bff26dd5f
ADOUKO Franck Venance <fvadouko@live.fr>
2022-07-18 17:31:37 +0000

    Integration of search page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -9,10 +9,25 @@ import { IoMdAdd } from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
 import useAuthStore from '../store/authStore';
 import { createOrGetUser } from '../utils';
+import { IUser } from '../types';
 
 const Navbar = () => {
-  const { addUser, removeUser } = useAuthStore();
-  const userProfile: any = useAuthStore((state) => state.userProfile);
+  const [user, setUser] = useState<IUser | null>();
+  const [searchValue, setSearchValue] = useState('');
+  const router = useRouter();
+  const { userProfile, addUser, removeUser } = useAuthStore();
+
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
+
+  const handleSearch = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+
+    if (searchValue) {
+      router.push(`/search/${searchValue}`);
+    }
+  };
 
   return (
     <div className="w-full border-b-2 flex justify-between items-center border-b-5 border-gray-200 border-solid py-2 px-4">
@@ -26,9 +41,27 @@ const Navbar = () => {
           />
         </div>
       </Link>
-      <div>BiSearch</div>
+      <div className="relative hidden md:block">
+        <form
+          onSubmit={handleSearch}
+          className="absolute md:static top-10 -left-20 bg-white"
+        >
+          <input
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            className="bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0"
+            placeholder="Search accounts and videos"
+          />
+          <button
+            onClick={handleSearch}
+            className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+          >
+            <BiSearch />
+          </button>
+        </form>
+      </div>
       <div>
-        {userProfile ? (
+        {user ? (
           <div className="flex gap-5 md:gap-10">
             <Link href="/upload">
               <button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
@@ -36,12 +69,12 @@ const Navbar = () => {
                 <span className="hidden md:block">Upload </span>
               </button>
             </Link>
-            {userProfile.image && (
-              <Link href={`/profile/${userProfile._id}`}>
+            {user.image && (
+              <Link href={`/profile/${user._id}`}>
                 <div>
                   <Image
                     className="rounded-full cursor-pointer"
-                    src={userProfile.image}
+                    src={user.image}
                     alt="user"
                     width={40}
                     height={40}
