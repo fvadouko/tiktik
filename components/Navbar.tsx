080742f718162feeff895f7719389ab42d3466c0
ADOUKO Franck Venance <fvadouko@live.fr>
2022-07-18 09:55:20 +0000

    styling elements

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
   const userProfile: any = useAuthStore((state) => state.userProfile);
 
   return (
-    <div className="w-full flex justify-between items-center border-b-5 border-gray-200 border-solid py-2 px-4">
+    <div className="w-full border-b-2 flex justify-between items-center border-b-5 border-gray-200 border-solid py-2 px-4">
       <Link href="/">
         <div className="w-[100px] md:w-[129px] md:h-[30px] h-[38px]">
           <Image
